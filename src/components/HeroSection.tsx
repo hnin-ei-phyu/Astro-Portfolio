2b6142f8341e95e4dd89b360d13ef39f30e9f967
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2025-12-10 12:18:17 +0700

    upd

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
-import { Download, Sparkle } from 'lucide-react'
+import { Download, } from 'lucide-react'
 
 
 
